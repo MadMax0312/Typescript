9d254aa751ae9fa8ac5accbc06a75780d1849c17
MadMax0312 <thahirmuhammedap@gmail.com>
2024-04-02 11:53:57 +0530

    Array

---
+++ myArrays.ts
@@ -0,0 +1,21 @@
+const heros: string[] = []
+const studentId: number[] = []
+
+const studentName: Array<string> = []
+const classNumber: Array<number> = []
+
+type User = {
+    name: string;
+    isActive: boolean;
+}
+
+const allUsers: User[] = []
+
+const MLModels: number[][] = [
+    [255, 255, 255],
+    []
+]
+
+studentId.push(2)
+heros.push('batman')
+allUsers.push({name:'jack', isActive:true})
