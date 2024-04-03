45e7ada0d58cb9ae380b5b08f5ef6b567e6ffcc1
MadMax0312 <thahirmuhammedap@gmail.com>
2024-04-03 12:08:36 +0530

    diff btw alias and interface

--- myInterface.ts
+++ myInterface.ts
@@ -1,20 +1,44 @@
 interface User {
     readonly dbId: number,
     name: string,
-    userId: number,
+    userId?: number,
     googleToken?: number,
     // startTrail: () => string
-    startTrail(): string,
-    getCoupon(couponName: string, value: number): number
+    startTrail?(): string,
+    getCoupon?(couponName: string, value: number): number
+}
+
+// Interface Reopening......
+interface User {
+    githubToken?: string
+}
+
+// Inheritance Property.........
+interface Admin extends User {
+    role: "admin" | 'ta' | 'learner'
+}
+
+const admin: Admin = {
+    role: 'admin',
+    dbId: 5,
+    name: 'Jim',
 }
 
 const max: User = {dbId: 3,name:'jack',userId:3,
         startTrail: ()=> {
             return 'trail started'
         },
-        getCoupon: (name: 'max12', off: 10) => {
-            return 10
-        }
+        getCoupon: (name: 'max12', off: 10) => 10
 }
 
 max.name = 'sara'
+
+// Alias extending property
+// but cannot be reopened to add new property
+type tempClass = {
+    name: string
+}
+
+type tempSchool = tempClass & {
+    age: number
+}
