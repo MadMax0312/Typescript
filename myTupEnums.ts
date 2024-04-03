5f2d1d770ae3a1245f356a63365a1a9b36662502
MadMax0312 <thahirmuhammedap@gmail.com>
2024-04-03 10:30:12 +0530

    tuples

---
+++ myTupEnums.ts
@@ -0,0 +1,16 @@
+const user: (string | number)[] = ['hello']
+
+let user1: [string, number, boolean] 
+
+user1 = ['hai',2,true]
+
+let rgb: [number, number, number] = [244,255,2]
+
+type User = [number, string]
+
+let newUser: User = [34, 'jack']
+
+newUser.push(55)
+
+export {}
+
