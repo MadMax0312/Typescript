ee493ced4d879b1c9456eaab4080d69d72e54906
MadMax0312 <thahirmuhammedap@gmail.com>
2024-04-16 14:43:39 +0530

    classes in typescript

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,10 @@
 "use strict";
-console.log('Typescript is here');
+class User {
+    constructor(email, name) {
+        this.city = 'calicut';
+        this.email = email;
+        this.name = name;
+    }
+}
+const obj = new User("t@gmail.com", "max");
+// obj.city = 'dd'
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Typescript is here')
+class User {
+    email: string
+    name: string
+    readonly city: string = 'calicut'
+    constructor(email: string, name: string) {
+        this.email = email;
+        this.name = name;
+    }
+}
+
+const obj = new User("t@gmail.com", "max")
+
+// obj.city = 'dd'
